cb9146234a874e3771d50d3cea6760123a50fb85
minghao.ong.2014 <minghao.ong.2014@sis.smu.edu.sg>
2017-03-18 20:15:16 +0800

    iot dashboard version 1.2

--- IOT/EyeOTBackend/web/js/main1.js
+++ IOT/EyeOTBackend/web/js/main1.js
@@ -13,10 +13,7 @@ $('.datepicker').datepicker({
 
 $(document).ready(function () {
     var d = new Date();
-    $(".datepicker").val(returnCorrectDate(d));
-    
-    $('[data-toggle="tooltip"]').tooltip(); 
-
+    $(".datepicker").val(returnCorrectDate(d))
 
     setInterval(function () {
         var today = new Date();
@@ -28,7 +25,7 @@ $(document).ready(function () {
 function returnCorrectDate(date) {
     var day = date.getDate();
     var month = date.getMonth();
-    var year = date.getFullYear() - 2000;
+    var year = date.getFullYear()-2000;
 
     switch (month) {
     case 0:
@@ -80,19 +77,19 @@ function returnCorrectTime(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var sec = date.getSeconds();
-
+    
     if (hours < 10) {
         hours = "0" + hours
     }
-
+    
     if (minutes < 10) {
         minutes = "0" + minutes
     }
-
+    
     if (sec < 10) {
         sec = "0" + sec
     }
-
+    
     return (hours + ":" + minutes + ":" + sec)
 }
 
@@ -103,23 +100,22 @@ function returnCorrectTime(date) {
 
 
 //**********************************
-//D3 - HeatMap
+//D3 - Line Graph
 //**********************************
 var margin = {
-        top: 20,
+        top: 50,
         right: 30,
         bottom: 0,
         left: 50
     },
-    width = 1080 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom,
+    width = 920 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom,
     gridSize = Math.floor(width / 24),
-    legendElementWidth = (width / 6) - 90,
+    //legendElementWidth = gridSize * 1.1,
     buckets = 9,
-    colors = ['#eff3ff', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594'],
-    oppcolors = ['#000000', '#000000', '#000000', '#000000', '#ffffff', '#ffffff', '#ffffff'],
-    days = ["Exit 1", "Exit 2", "Exit 3", "Exit 4", "Exit 5", "Exit 6"],
-    times = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
+    colors = ['#eff3ff', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594'];
+days = ["Exit 1", "Exit 2", "Exit 3", "Exit 4", "Exit 5", "Exit 6", "Exit 7"];
+times = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
 
 var svg = d3.select("#heatMap").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -210,6 +206,8 @@ var heatmapChart = function () {
 
             cards.exit().remove();
 
+            /*
+            ***********REMOVE LEGENDS FOR NOW*************************
             var legend = svg.selectAll(".legend")
                 .data([0].concat(colorScale.quantiles()), function (d) {
                     return d;
@@ -222,7 +220,7 @@ var heatmapChart = function () {
                 .attr("x", function (d, i) {
                     return legendElementWidth * i;
                 })
-                .attr("y", height - 20)
+                .attr("y", height - 60)
                 .attr("width", legendElementWidth)
                 .attr("height", gridSize / 2)
                 .style("fill", function (d, i) {
@@ -235,79 +233,14 @@ var heatmapChart = function () {
                     return "≥ " + Math.round(d);
                 })
                 .attr("x", function (d, i) {
-                    return legendElementWidth * i + 50;
+                    return legendElementWidth * i;
                 })
-                .attr("y", height - 5)
-                .style("fill", function (d, i) {
-                    return oppcolors[i];
-                });
+                .attr("y", height - 70);
 
             legend.exit().remove();
-
+            **********************************************************
+            */
 
         });
 };
-
 heatmapChart();
-
-//**********************************
-//D3 - Line Graph
-//**********************************
-/*var margin = {
-        top: 20,
-        right: 30,
-        bottom: 0,
-        left: 50
-    },
-    width = 1080 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom
-
-var lineGraph = function () {
-    d3.csv("../data/faketimedata.csv",
-        function (d) {
-            return {
-                date: +d.date,
-                stolen: +d.stolen,
-                triggered: +d.triggered
-            };
-        },
-        function (error, data) {
-            var colorScale = d3.scale.quantile()
-                .domain([0, buckets - 1, d3.max(data, function (d) {
-                    return d.value;
-                })])
-                .range(colors);
-
-            var cards = svg.selectAll(".hour")
-                .data(data, function (d) {
-                    return d.exit + ':' + d.hour;
-                });
-
-            cards.append("title");
-
-            cards.enter().append("rect")
-                .attr("x", function (d) {
-                    return (d.hour - 1) * gridSize;
-                })
-                .attr("y", function (d) {
-                    return (d.exit - 1) * gridSize;
-                })
-                .attr("rx", 4)
-                .attr("ry", 4)
-                .attr("class", "hour bordered")
-                .attr("width", gridSize)
-                .attr("height", gridSize)
-                .style("fill", colors[0])
-                .on('mouseover', tip.show)
-                .on('mouseout', tip.hide);
-
-            cards.transition().duration(1000)
-                .style("fill", function (d) {
-                    return colorScale(d.value);
-                });
-
-            cards.exit().remove();
-        });
-}
-
-lineGraph();*/
