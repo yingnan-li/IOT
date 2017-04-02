9b8ecc194c04e7b2f459bd09e421962c90d56522
tankunsheng <kunshengtan.2014@smu.edu.sg>
2017-04-02 15:15:05 +0800

    2-4-2017 3:15pm

---
+++ IOT/EyeOTBackend_ver2/EyeOTBackend_ver2/src/java/DAO/PiDAO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+import Entity.Officer;
+import Entity.Pi;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author user
+ */
+public class PiDAO {
+    private final String LOCATION_COL = "placed_location";
+    private final String TYPE_COL = "type";
+    
+    
+    public Pi getPiDetails(String piID){
+        
+        Pi pi = null;
+        String sqlQuery="SELECT * FROM pi where PiID = ?";
+        
+        try (Connection conn = ConnectionManager.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sqlQuery)) {
+            
+            ps.setString(1,piID);
+            
+            ResultSet results = ps.executeQuery();
+            while(results.next()){
+                String location = results.getString(LOCATION_COL); 
+                String type = results.getString(TYPE_COL); 
+                pi = new Pi(piID, location, type);
+            }
+            
+          
+        } catch (SQLException ex) {
+            Logger.getLogger(PiOnTrolleytoBeaconDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return pi;
+        
+    }
+}
---
+++ IOT/EyeOTBackend_ver2/EyeOTBackend_ver2/src/java/Entity/Pi.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author user
+ */
+public class Pi {
+    private String piID;
+    private String placed_location;
+    private String type;
+
+    public Pi(String piID, String placed_location, String type) {
+        this.piID = piID;
+        this.placed_location = placed_location;
+        this.type = type;
+    }
+
+    public String getPiID() {
+        return piID;
+    }
+
+    public void setPiID(String piID) {
+        this.piID = piID;
+    }
+
+    public String getPlaced_location() {
+        return placed_location;
+    }
+
+    public void setPlaced_location(String placed_location) {
+        this.placed_location = placed_location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+}
